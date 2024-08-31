03cd937091c8b065b635715550f6592cda53e8f4
Charles <78160523+CharlesD12@users.noreply.github.com>
2024-08-31 17:42:18 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,6 +1,5 @@
-pip install matplotlib
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
