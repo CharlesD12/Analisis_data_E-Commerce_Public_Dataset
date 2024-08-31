1a75ea648b6c1cf0a8bd1f4163a8bfc0a5b09648
Charles <78160523+CharlesD12@users.noreply.github.com>
2024-08-31 17:32:23 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,9 +1,3 @@
-import pandas as pd
-import seaborn as sns
-import streamlit as st
-from babel.numbers import format_currency
-sns.set(style='dark')
-
 #create customers_demographics_df
 def create_customer_bycity_df(df):
   bycity_df = df.groupby(by="customer_city").customer_id.nunique().reset_index()
